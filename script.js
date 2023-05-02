4066594fc193778f83f91c3d9c3cac6f0e757167
Shubham Kumar <shubham49557@gmail.com>
2023-05-02 15:20:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const amount = document.getElementById('amount');
+const guests = document.getElementById('guests');
+const quality = document.getElementById('quality');
+const tipAmount = document.getElementById('tip-amount');
+
+function calculate()
+{
+const tip =  ((amount.value*quality.value)/(guests.value)).toFixed(2);
+ console.log(tip)
+
+ amount.value = '';
+ guests.value = '';
+ quality.value = '';
+
+ if(tip==='NaN')
+ {
+    tipAmount.innerHTML = 'Ti[ $0 each';
+    showTipAmount();
+ }
+ else
+ {
+    tipAmount.innerHTML = 'Tip $' + tip + 'each';
+    showTipAmount();
+ }
+
+
+}
+
+showTipAmount = ()=>
+{
+    var x = tipAmount;
+    x.className = 'show';
+    setTimeout(function(){x.className= x.className.replace('show','');},3000);}
+
